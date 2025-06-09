2699d21435aa4da6ef0e3061c1e7110357b1b17c
Liam Edward Brem <liambrem@meta.com>
2025-06-09 16:24:45 -0700

    Displays popular movies

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
+import "./MovieList.css";
 
-const fetchData = async () => {
-  const apiKey = import.meta.env.VITE_API_KEY_;
-
+const fetchData = async (apiKey) => {
   const url =
-    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"; // We can change increment the page number to fetch more data
+    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
   const options = {
     method: "GET",
     headers: {
@@ -15,16 +14,31 @@ const fetchData = async () => {
   };
 
   const response = await fetch(url, options);
+  if (!response.ok) {
+    throw new Error("Failed to fetch movie data");
+  }
   const data = await response.json();
   return data;
 };
 
-const MovieList = () => {
-  let data = fetchData();
-  console.log(data);
+const MovieList = ({ searchCriteria }) => {
+  const [movieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    const apiKey = import.meta.env.VITE_API_KEY_;
+    fetchData(apiKey)
+      .then((data) => setMovieData(data.results))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div>
       <h1>Movie List</h1>
+      <div className="movie-list">
+        {movieData.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
     </div>
   );
 };

fbe8ed4c084ffcb72f977ae4facf4639e24c933f
Liam Edward Brem <liambrem@meta.com>
2025-06-10 11:59:11 -0700

    Load More

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 import MovieCard from "./MovieCard";
 import "./MovieList.css";
 
-const fetchData = async (apiKey) => {
+const fetchData = async (apiKey, pageNumber) => {
   const url =
-    "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
+    `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${pageNumber}`;
   const options = {
     method: "GET",
     headers: {
@@ -23,14 +23,15 @@ const fetchData = async (apiKey) => {
 
 const MovieList = ({ searchCriteria }) => {
   const [movieData, setMovieData] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
   
 
   useEffect(() => {
-    const apiKey = import.meta.env.VITE_API_KEY_;
-    fetchData(apiKey)
-      .then((data) => setMovieData(data.results))
+    const apiKey = import.meta.env.VITE_API_KEY;
+    fetchData(apiKey, pageNumber)
+      .then((data) => setMovieData((prev) => [...prev, ...data.results]))
       .catch((error) => console.error(error));
-  }, []);
+  }, [pageNumber]);
 
   return (
     <div>
@@ -39,6 +40,9 @@ const MovieList = ({ searchCriteria }) => {
         {movieData.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
+      </div>
+      <div className="button-wrapper">
+      <button className="load-more-button" onClick={() => {setPageNumber((pageNumber) => pageNumber + 1)}}>Load More</button>
       </div>
     </div>
   );

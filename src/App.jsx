28b55eef2d6417d0a5e4e7d5c2b3a0068c0a5424
Liam Edward Brem <liambrem@meta.com>
2025-06-11 09:09:02 -0700

    Implemented header & footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import MovieList from "./components/MovieList";
 //import Search from "./components/Search";
 import Sort from "./components/Sort";
 import Search from "./components/Search";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   const [search, setSearch] = useState("");
@@ -11,11 +13,13 @@ const App = () => {
 
   return (
     <div className="App">
+      <Header />
       <div className="controls" >
         <Search onSearch={setSearch}/>
         <Sort value={sort} onChange={setSort} />
       </div>
       <MovieList searchCriteria={search} sortCriteria={sort} />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+   <body class="title-bg">
+  <div class="title-container">
+    <h1 class="title-text" data-text="FLIXSTER">FLIXSTER</h1>
+    <div class="rain"></div>
+  </div>
+</body>
+  );
+};
+
+export default Header;

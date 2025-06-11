60cc97b1d4fbc4290c7241cd966b17d9c69ec44b
Liam Edward Brem <liambrem@meta.com>
2025-06-11 16:07:08 -0700

    SideBar functionality

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import "./MovieCard.css";
 import Modal from "./Modal";
 
+import { VscHeart, VscHeartFilled, VscEye, VscEyeClosed } from "react-icons/vsc";
+
+
 // determine the color based on the rating (0-10)
 const getColor = (rating) => {
   const value = Math.max(0, Math.min(10, rating));
@@ -9,20 +12,45 @@ const getColor = (rating) => {
   return `hsl(${hue}, 75%, 45%)`;
 };
 
-const MovieCard = ({ movie }) => {
+const MovieCard = ({ movie, isFavorite, isWatched, updateWatched, updateFavorites }) => {
   const [displayModal, setDisplayModal] = React.useState(false);
+
 
   return (
     <>
       <article className="movie-card" onClick={() => setDisplayModal(true)}>
         <h1 className="movie-title">{movie.title}</h1>
-        <h4
-          className="movie-rating"
-          style={{ color: getColor(movie.vote_average) }}
-        >
-          {movie.vote_average.toFixed(1)}
-        </h4>
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.title} Poster`} />
+        <div className="bottom-card-row">
+          <div
+            className={`favorite-button ${isFavorite ? "active" : ""}`}
+            onClick={(e) => {
+              e.stopPropagation();
+              updateFavorites();
+            }}
+          >
+            {isFavorite ? <VscHeartFilled /> : <VscHeart />}
+          </div>
+
+          <h4
+            className="movie-rating"
+            style={{ color: getColor(movie.vote_average) }}
+          >
+            {movie.vote_average.toFixed(1)}
+          </h4>
+          <div
+            className={`watched-button ${isWatched ? "active" : ""}`}
+            onClick={(e) => {
+              e.stopPropagation();
+              updateWatched();
+            }}
+          >
+            {isWatched ? <VscEye /> : <VscEyeClosed />}
+          </div>
+        </div>
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={`${movie.title} Poster`}
+        />
       </article>
       <Modal
         show={displayModal}
